e85380c75aa775e28bc7bf43a1d15e0772491005
ialexeyev <98263107+ialexeyev@users.noreply.github.com>
2024-03-26 05:08:26 +0000

    V1.02

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from cmap import create_app
 
-application = create_app()
+app = create_app()
 
 if (__name__ == '__main__'):
-  application.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
